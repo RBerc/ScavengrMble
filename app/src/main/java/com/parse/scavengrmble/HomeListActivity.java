2e214382bed670bb61e944d4db8f5a16d2949e8a
Rob <rbercovic@gmail.com>
2015-04-12 15:13:48 -0400

    Drawable Resources included. Can build HomeListActivity (although Blank)

--- app/src/main/java/com/parse/scavengrmble/HomeListActivity.java
+++ app/src/main/java/com/parse/scavengrmble/HomeListActivity.java
@@ -11,7 +11,7 @@ public class HomeListActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_list_view);
+        setContentView(R.layout.activity_home_list);
     }
 
 
